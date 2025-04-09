415c6ceb290a463911eba54f35ed008b8c73b0c5
varunbajaj02 <varunbajaj3690@gmail.com>
2025-04-09 16:09:02 +0530

    exam

---
+++ cie.java
@@ -0,0 +1,20 @@
+package cie;
+
+public class cie {
+    public static void main(String[] args) {
+        int a=-1,b=1;
+        if(a<0){
+            System.out.println(a+"is negetive");
+        }
+        else{
+            System.out.println(a+"is positive ");
+        }
+    
+    if(b<0){
+        System.out.println(b +"is negetive");
+    }
+    else{
+        System.out.println(b +"is positive ");
+    }
+}
+}
